b3a947f6096e5af11880d192582929069a06ade1
anas boutbagha <lazawianas@gmail.com>
2026-09-19 16:04:24 +0100

    add exercies of days one

---
+++ day2/ex9.js
@@ -0,0 +1,9 @@
+const p = require("prompt-sync")()
+let l = Number(p ("give me the length of the array "))
+let t = [l-1]
+for(let i = 0;i < l; i++)
+    {
+const p = require("prompt-sync")()
+t[i] = Number(p (`give me a number dans la case ${i}` ))
+}
+console.log(t)
